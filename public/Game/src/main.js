50ac04a4c2ef6f536abd1a449a8d3512d870cdda
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2021-12-11 19:55:56 +0000

    EDITED PLAY AND DESIGN TEXT

--- public/Game/src/main.js
+++ public/Game/src/main.js
@@ -29,9 +29,6 @@ window.addEventListener('load', function () {
     game.scene.add("Level04", Level04);
     game.scene.add("Level05", Level05);
     game.scene.add("Level06", Level06);
-    //game.scene.add("Level", Level);
-    //game.scene.add("Level1", Level1);
-    //game.scene.add("LevelTest", LevelTest);
     game.scene.add("Boot", Boot, true);
 });
 
